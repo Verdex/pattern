fc5c747dbf563a57e64b4a10dff015ec8d381618
David L. Inman <davidlevere@gmail.com>
2022-02-10 21:40:45 -0500

    inc prog

--- src/generation/generator.rs
+++ src/generation/generator.rs
@@ -2,6 +2,9 @@
 use crate::ast::Ast;
 use crate::ir::Ir;
 
-pub fn generate( asts : Vec<Ast> ) -> Vec<Ir> {
-    vec![]
+use super::data::StaticError;
+use super::type_info;
+
+pub fn generate( asts : Vec<Ast> ) -> Result<Vec<Ir>, StaticError> {
+    Ok(vec![])
 }
