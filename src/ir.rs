7186313847eb23f134ea739fda8a41ca1413d8f1
David L. Inman <davidlevere@gmail.com>
2022-02-12 18:34:32 -0500

    ir

--- src/ir.rs
+++ src/ir.rs
@@ -1,6 +1,27 @@
 
+#[derive(Debug)]
 pub struct Ir {
+    pub name : Symbol,
+    pub params : Vec<Symbol>,
+    pub statements : Vec<Statement>,
+}
 
+#[derive(Debug)]
+pub enum Statement {
+    Assign { name : Symbol, expr : Box<Expr> },
+    If { test : Symbol, statements : Vec<Statement> },
+    Label(Symbol),
+    Goto(Symbol),
+    Return(Symbol),
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Number(i64),
+    Bool(bool),
+    Variable(Symbol),
+    SlotAccess { data : Symbol, slot : Symbol }, 
+    FunCall { name : Symbol, params : Vec<Symbol> },
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
