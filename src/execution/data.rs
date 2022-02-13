50d21b78ec271e9dd0af4e30cafd2c983b58ac4b
David L. Inman <davidlevere@gmail.com>
2022-02-13 11:41:58 -0500

    vm setup

--- src/execution/data.rs
+++ src/execution/data.rs
@@ -1,5 +1,21 @@
 
-#[derive(Debug)]
-enum Instr {
+use crate::ir::Symbol;
 
+#[derive(Debug)]
+pub enum Instr {
+
+}
+
+#[derive(Debug)]
+pub enum Ref {
+    Heap(usize),
+    Fun { fun_address : usize, environment_address : Option<usize> ),
+}
+
+#[derive(Debug)]
+pub enum Data {
+    Bool(bool),
+    Number(i64),
+    Compound { tag : Symbol, params: Vec<Data> },
+    Reference(Ref),
 }
--- src/execution/vm.rs
+++ src/execution/vm.rs
@@ -3,8 +3,15 @@ use std::collections::HashMap;
 
 use crate::ir::Ir;
 
+use super::data::{Instr, Data, Ref};
 
 
 pub fn run( ir : Vec<Ir> ) {
-    
+    let instructions : Vec<Instr> = vec![]; 
+    let mut ip : usize = 0;
+
+    let mut stack : Vec<Ref> = vec![];
+    let mut sp : usize = 0; 
+
+    let mut heap : Vec<Data> = vec![];
 }
