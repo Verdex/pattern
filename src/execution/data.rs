df8d2d3551243e95b6060667bea87697c3b4c9bb
David L. Inman <davidlevere@gmail.com>
2022-02-19 17:22:39 -0500

    data

--- src/execution/data.rs
+++ src/execution/data.rs
@@ -1,11 +1,23 @@
 
+use super::instr::InstructionAddress;
+
 #[derive(Debug, Clone, Copy)]
 pub struct HeapAddress(pub usize);
+
 
 #[derive(Debug)]
 pub enum Data {
     Bool(bool),
     Number(i64),
     String(String),
+    Func(InstructionAddress),
     Ref(HeapAddress),
+}
+
+#[derive(Debug)]
+pub struct Frame {
+    pub incoming_params : Vec<HeapAddress>,
+    pub outgoing_params : Vec<HeapAddress>,
+    pub return_address : Option<InstructionAddress>,
+    pub stack : Vec<HeapAddress>,
 }
