1067513d82c1f1b72e9e88f695f5a90e3bee59f1
David L. Inman <davidlevere@gmail.com>
2022-02-19 20:10:05 -0500

    inc prog instr.rs

--- src/execution/instr.rs
+++ src/execution/instr.rs
@@ -1,12 +1,19 @@
 
 #[derive(Debug, Clone, Copy)]
-pub struct StackOffset(pub usize)
+pub struct StackOffset(pub usize);
 
 #[derive(Debug, Clone, Copy)]
-pub struct InstructionAddress(pub usize)
+pub struct InstructionAddress(pub usize);
+
+impl InstructionAddress {
+    pub fn next(&self) -> InstructionAddress {
+        InstructionAddress(self.0 + 1)
+    }
+}
 
 #[derive(Debug)]
 pub enum Instruction { 
    Print(StackOffset),
    Call(InstructionAddress),
+   Exit,
 }
