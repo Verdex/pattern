0464a20cbbb5e4c305cf4184b63886d96a33cb0d
David L. Inman <davidlevere@gmail.com>
2022-02-22 18:58:14 -0500

    clean up comments

--- src/generation/type_info.rs
+++ src/generation/type_info.rs
@@ -23,40 +23,13 @@ pub fn ast_to_ir_type(t : ast::Type) -> Type {
     let array = ConcreteType("Array".to_string());
 
     match t {
-        //ast::Type::Generic(name) => super::data::Type::Generic(name),
-        //ast::Type::Concrete(name) => super::data::Type::Concrete(ConcreteType(name)),
-        //ast::Type::Array(t) => super::data::Type::Index { name: array.clone(), params: vec![ast_to_ir_type(*t)] },
-        //ast::Type::Fun { input, output } => super::data::Type::Fun { input: m(input), output: Box::new(ast_to_ir_type(*output)) },
-        //ast::Type::Index { name, params } => super::data::Type::Index { name: ConcreteType(name), params: m(params) },
-        _ => panic!("!"),
-    }
-
-    /*match t {
         ast::Type::Generic(name) => super::data::Type::Generic(name),
         ast::Type::Concrete(name) => super::data::Type::Concrete(ConcreteType(name)),
         ast::Type::Array(t) => super::data::Type::Index { name: array.clone(), params: vec![ast_to_ir_type(*t)] },
         ast::Type::Fun { input, output } => super::data::Type::Fun { input: m(input), output: Box::new(ast_to_ir_type(*output)) },
         ast::Type::Index { name, params } => super::data::Type::Index { name: ConcreteType(name), params: m(params) },
-    }*/
+    }
 }
-/*pub fn ast_to_ir_type(t : ast::Type) -> Type {
-
-    use ast::Type as a;
-
-    fn m(ts : Vec<ast::Type>) -> Vec<Type> {
-        ts.into_iter().map(ast_to_ir_type).collect()
-    }
-
-    let array = ConcreteType("Array".to_string());
-
-    match t {
-        a::Generic(name) => Type::Generic(name),
-        a::Concrete(name) => Type::Concrete(ConcreteType(name)),
-        a::Array(t) => Type::Index { name: array.clone(), params: vec![ast_to_ir_type(*t)] },
-        a::Fun { input, output } => Type::Fun { input: m(input), output: Box::new(ast_to_ir_type(*output)) },
-        a::Index { name, params } => Type::Index { name: ConcreteType(name), params: m(params) },
-    }
-}*/
 
 pub fn determine_type_info( data_defs : Vec<ast::Ast> ) 
     -> Result<( HashMap<ConsTag, ConcreteType>
